ecdf75c754ceffaf8469c748a767822e65e5d849
mmehdiali5 <mmehdiali5@gmail.com>
2023-05-31 20:36:26 -0400

    Login integrated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
 
-import React from "react";
+import React, {useState} from "react";
 import Card from "./card";
+import Home from "./home";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router"
 
 const App = () => {
-  return (
-    <div className="login-outdivcard">
-      <Card></Card>
-    </div>
-  );
+    const [token, setToken] = useState();
+    if (!token) {
+        return (
+            <div className="login-outdivcard">
+                <Card setToken={setToken}/>
+            </div>
+        )
+    }
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/"
+                       element={<Home/>}/>
+                <Route path="/home/*"
+                       element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/card.js
+++ src/card.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import PropTypes from 'prop-types';
 import "./login.css";
-const card = () => {
+
+async function loginUser(credentials) {
+    console.log(JSON.stringify(credentials))
+    return fetch('http://206.189.181.234:8087/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+const Card = ({ setToken }) => {
+    let [username, setUsername] = useState('');
+    let [password, setPassword] = useState('');
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+        console.log(token);
+    }
+
   return (
     <div className="login-innerCard">
       <h1>Strava - Share what you do !</h1>
-      <input type="text" placeholder="Name"></input>
-      <input type="password" placeholder="Password"></input>
+      <input value={username} type="text" placeholder="Name"
+             onChange={(event) => setUsername(event.target.value)}/>
+      <input value={password} type="password" placeholder="Password"
+             onChange={(event) => setPassword(event.target.value)}/>
       <div className="cardbuttons">
-        <button className="loginbutton">Login</button>
+        <button className="loginbutton" onClick={handleSubmit}>Login</button>
         <button className="loginbutton">Signup</button>
       </div>
     </div>
   );
 };
 
-export default card;
+Card.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+export default Card;
