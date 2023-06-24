6a017aabf629b02278dc202a91ef0a465cea7df9
mmehdiali5 <mmehdiali5@gmail.com>
2023-06-24 15:24:12 -0400

    Search API integrated. Search details

--- src/Search/searchapi.js
+++ src/Search/searchapi.js
@@ -5,12 +5,14 @@ import {AiOutlineSearch} from "react-icons/ai";
 import Feed from "../Feed/feed";
 import "../Feed/feeds.css";
 import axios from "axios";
+
 const api = axios.create();
 
 const SearchApi = () => {
     const navigate = useNavigate();
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+    const [details, setDetails] = useState(null);
 
     const handleSearch = (e) => {
         if (e.key === "Enter") {
@@ -28,9 +30,21 @@ const SearchApi = () => {
                 'X-Api-Key': 'qs5WVpBDq5Hm1QxE7W8qnQ==neepvZyKo05PG6Sb'
             }
         });
-        results= response.data;
+        results = response.data;
         setSearchResults(results);
+        setDetails(null);
     };
+
+    const handleDetails = async (name) => {
+        console.log(name);
+        const response = await api.get(`https://api.api-ninjas.com/v1/exercises?name=${name}`, {
+            headers: {
+                'X-Api-Key': 'qs5WVpBDq5Hm1QxE7W8qnQ==neepvZyKo05PG6Sb'
+            }
+        });
+        setDetails(response.data);
+        setSearchResults([]);
+    }
 
     return (
         <>
@@ -43,9 +57,11 @@ const SearchApi = () => {
                 </div>
                 <div className="col-1"></div>
                 <div className="col-6">
+                    <p>Allowed Types: (Cardio, olympic_weightlifting, plyometrics, powerlifting,
+                        strength, stretching, strongman)</p>
                     <div className="col-12 col-md-11 position-relative">
                         <input
-                            placeholder="Search Workouts"
+                            placeholder="Search Workouts By Type"
                             className="form-control rounded-pill pe-5 ps-5 search-input"
                             onKeyDown={handleSearch}
                         />
@@ -57,17 +73,20 @@ const SearchApi = () => {
                             <h4>Search Results for "{searchQuery}":</h4>
                             {/* Render the search query */}
                             &nbsp;
-                            <p>{searchQuery}</p>
                             {/* Render the search results */}
                             {searchResults.length > 0 ? (
                                 <ul>
                                     {searchResults.map((result) => (
-                                        <li key={result.id}>{JSON.stringify(result)}</li>
+                                        <li key={result.name}><div style={{"border":"1px solid black"}}
+                                            onClick={() => handleDetails(result.name)}>{result.name}</div></li>
                                     ))}
                                 </ul>
                             ) : null}
                         </div>
                     )}
+                    {
+                        details && <ul>{details.map((detail)=><li>{JSON.stringify(detail)}</li>)}</ul>
+                    }
                 </div>
             </div>
         </>
